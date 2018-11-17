2f65117143f7832dbe5059b1c213daaf89415916
Vamsi Krishna Bezavada <vbezavada.zteam@zuora.com>
2018-11-17 12:31:06 -0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Form from "./components/Form";
+import Recipes from "./components/Recipes";
+
+
+const API_KEY = '144416157fd05829a7bd2a947ada373a';//Get your own Api key from food2fork by creating account in that website
+
+class App extends Component {
+  state = {
+    recipes: []
+  }
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=1000`);
+    
+    const results = await api_call.json();
+    this.setState({ recipes: results.recipes });
+    console.log(this.state.recipes);
+  }
+  componentDidMount = () => {
+    const json = localStorage.getItem("recipes");
+    const recipes = JSON.parse(json);
+    this.setState({ recipes });
+  }
+  componentDidUpdate = () => {
+    const recipes = JSON.stringify(this.state.recipes);
+    localStorage.setItem("recipes", recipes);
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">E-Recipes</h1>
+        <span><i className="title-name">Welcome To e-Recipes Page, Please Type a Key Word: Example -"Chicken","Shrimp" Etc.</i></span>
+        </header>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes} />
+      </div>
+    );
+  }
+}
+
+export default App;
